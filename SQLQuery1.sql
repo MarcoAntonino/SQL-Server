4f7e5a2f7e3d03ac76270c56b33e682437002bf1
Marco Antonino <antoninom@AULE.IT>
2017-05-19 17:54:31 +0200

    creata qualche tabella

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,3 +8,48 @@ CREATE TABLE Categoria (
     Immagine varchar(50)
 )
 go
+
+CREATE TABLE Prodotto (
+    ID int NOT NULL PRIMARY KEY identity(1,1),
+    Nome varchar NOT NULL,
+    Descrizione varchar(255)NOT NULL,
+    Prezzo float NOT NULL,
+    Giacenza int NOT NULL,
+    IDCategoria int NOT NULL,
+    FOREIGN KEY(IDCategoria) REFERENCES Categoria(ID)
+)
+go
+
+CREATE TABLE Cliente (
+    ID int NOT NULL PRIMARY KEY identity(1,1),
+    Nome varchar(50) NOT NULL,
+    Cognome varchar(50)NOT NULL,
+    CodiceFiscale varchar(20) NOT NULL,
+    Partitaiva varchar(20)NOT NULL,
+    RagioneSociale varchar(50)NOT NULL,
+    Email varchar(20)NOT NULL,
+    Telefono varchar(50)NOT NULL
+)
+go
+
+CREATE TABLE Gallery (
+	ID int NOT NULL PRIMARY KEY identity(1,1),
+	Immagine varchar(255) NOT NULL,
+	Didascalia varchar(100),
+	IDProdotto int NOT NULL,
+	FOREIGN KEY(IDProdotto) REFERENCES Prodotto(ID)
+)
+go
+
+CREATE TABLE Indirizzo (
+    ID int NOT NULL PRIMARY KEY identity(1,1),
+    Via varchar(80) NOT NULL,
+    Citta varchar(80) NOT NULL,
+    Provincia varchar(80) NOT NULL,
+    Paese varchar(80) NOT NULL,
+    IDCliente int NOT NULL,
+    FOREIGN KEY(IDCliente) REFERENCES Cliente(ID)
+
+)
+go
+
